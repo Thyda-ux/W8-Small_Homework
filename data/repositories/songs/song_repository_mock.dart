302cd55127ab8ff3973a11f5c086311d1ad95474
Thyda-ux <sothyda.mao@gmail.com>
2026-03-11 21:10:42 +0700

    Changed to async value

--- data/repositories/songs/song_repository_mock.dart
+++ data/repositories/songs/song_repository_mock.dart
@@ -1,5 +1,3 @@
-// song_repository_mock.dart
-
 import '../../../model/songs/song.dart';
 import 'song_repository.dart';
 
@@ -40,18 +38,18 @@ class SongRepositoryMock implements SongRepository {
   @override
   Future<List<Song>> fetchSongs() async {
     await Future.delayed(Duration(minutes: 2), () {});
- 
+
     return _songs;
   }
 
   @override
   Future<Song?> fetchSongById(String id) async {
-    // - Simulate a delay of 3 seconds.
+    await Future.delayed(const Duration(seconds: 3));
 
-    // - After the delay : Find the song of given id in the list of songs and return it
-
-    // - If not found : Throw an error with the message “no song found for id 25 in the database"
-
-    return Future.delayed(Duration.zero); // TO CHANGE !
+    try {
+      return _songs.firstWhere((song) => song.id == id);
+    } catch (e) {
+      throw Exception('no song found for id $id in the database');
+    }
   }
 }
